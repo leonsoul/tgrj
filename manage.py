481a606101ac0923c8633aeb76a81c428dfc7426
Bene <wangben@alltuu.com>
2023-01-14 23:44:41 +0800

    增加日志文件

--- manage.py
+++ manage.py
@@ -1,9 +1,12 @@
 from flask_script import Manager
 from Demo_utils.app import create_app
+from Demo_utils.logger import Log as log
 
 app = create_app()
 manage = Manager(app)
 
-
 if __name__ == '__main__':
-    manage.run()
+    try:
+        manage.run()
+    except Exception as e:
+        log.error("启动异常", e)
