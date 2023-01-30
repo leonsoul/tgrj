d5176a836272a2913cdb0f56b4e5b2fe06c7c6cb
Bene <wangben@alltuu.com>
2023-01-30 16:00:52 +0800

    去掉注释

--- manage.py
+++ manage.py
@@ -1,12 +1,9 @@
 from flask_script import Manager
+
 from Demo_utils.app import create_app
-from Demo_utils.logger import Log as log
 
 app = create_app()
 manage = Manager(app)
 
 if __name__ == '__main__':
-    # try:
     manage.run()
-    # except Exception as e:
-    #     log.error("启动异常", e)
